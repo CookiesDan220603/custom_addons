7896378ef8f7abc6eb3dcc1078b936f0b8c2b322
Elsie <52100916@student.tdtu.edu.vn>
2026-01-10 14:50:11 +0700

    add

--- co_sale_reduce_action/models/sale_order_line.py
+++ co_sale_reduce_action/models/sale_order_line.py
@@ -10,10 +10,7 @@ class SaleOrderLine(models.Model):
     out_of_stock = fields.Boolean(string='Out of Stock',
                                   compute='_compute_out_of_stock',
                                   store=True)
-    product_category_id = fields.Many2one(
-        'product.category',
-        string='Phân loại sản phẩm',
-    )
+    
     # Compute
     @api.depends('product_uom_qty', 'product_template_id', 'order_id.order_line.product_uom_qty')
     def _compute_out_of_stock(self):
@@ -32,23 +29,7 @@ class SaleOrderLine(models.Model):
                 for l in same_product_lines:
                     l.out_of_stock = False
 
-    # ========= ONCHANGE =========
-    @api.onchange('product_category_id')
-    def _onchange_product_category_id(self):
-        if self.product_category_id:
-            # Lấy cả category con
-            category_ids = self.env['product.category'].search([
-                ('id', 'child_of', self.product_category_id.id)
-            ]).ids
-
-            domain = [
-                ('sale_ok', '=', True),
-                ('categ_id', 'in', category_ids)
-            ]
-        else:
-            domain = [('sale_ok', '=', True)]
-
-        return {'domain': {'product_template_id': domain}}
+    
 
 
 
